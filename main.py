8ff9f9610cebbd8c262c68ae05ee3d5d17489b92
is-9108 <syouta103010@gmail.com>
2020-08-13 21:03:50 +0900

    second commit

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 from apiclient.discovery import build
+import pandas as pd
 
-YOUTUBE_API_KEY = 'AIzaSyDqZZK_zsS-zoJd7ybGI1cjmlwRqFFENzE'
+YOUTUBE_API_KEY = 'api_key'
 
 youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
 
-def get_info(keyword):
-    search_response = youtube.search().list(
-        part='id,snippet',
-        #検索したい文字列を指定
-        q=keyword,
-        #視聴回数が多い順に取得
-        # order='viewCount',
-        # type='video',
-    ).execute()
+def get_video_info(part, q, order, type, num):
+    dic_list = []
+    search_response = youtube.search().list(part=part,q=q,order=order,type=type)
+    output = youtube.search().list(part=part,q=q,order=order,type=type).execute()
 
-    return search_response
+    #一度に5件しか取得できないため何度も繰り返して実行
+    for i in range(num):        
+        dic_list = dic_list + output['items']
+        search_response = youtube.search().list_next(search_response, output)
+        output = search_response.execute()
 
-keyword = input('検索キーワードを入力：')
-search_response = get_info(keyword)
+    df = pd.DataFrame(dic_list)
+    #各動画毎に一意のvideoIdを取得
+    df1 = pd.DataFrame(list(df['id']))['videoId']
+    #各動画毎に一意のvideoIdを取得必要な動画情報だけ取得
+    df2 = pd.DataFrame(list(df['snippet']))[['channelTitle','publishedAt','channelId','title','description']]
+    ddf = pd.concat([df1,df2], axis = 1)
 
-for sr in search_response.get('items',[]):
-    print(sr['snippet']['title'])
-    print(sr['snippet']['channelTitle'])
-    print('\n')
+    ddf.to_csv("youtube.csv")
+
+    return ddf
+
+get_video_info(part = 'snippet',q = 'dbd',order = 'viewCount',type = 'video',num = 20)
+
+
